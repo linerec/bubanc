16da55e124faca03d240a685e03bba3430695380
Ubuntu <ubuntu@ip-172-31-40-98.us-east-2.compute.internal>
2020-07-14 22:11:26 +0000

    Added New Files

---
+++ application/logs/log-2020-07-15.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-07-15 00:00:23 --> Severity: Warning  --> opendir(/home/ubuntu/bubanc/templates/selio/components): failed to open dir: No such file or directory /home/ubuntu/bubanc/application/controllers/frontend.php 5045
+ERROR - 2020-07-15 00:00:23 --> Severity: Warning  --> readdir() expects parameter 1 to be resource, bool given /home/ubuntu/bubanc/application/controllers/frontend.php 5048
+ERROR - 2020-07-15 00:00:28 --> Severity: Warning  --> opendir(/home/ubuntu/bubanc/templates/selio/components): failed to open dir: No such file or directory /home/ubuntu/bubanc/application/controllers/frontend.php 5045
+ERROR - 2020-07-15 00:00:28 --> Severity: Warning  --> readdir() expects parameter 1 to be resource, bool given /home/ubuntu/bubanc/application/controllers/frontend.php 5048
