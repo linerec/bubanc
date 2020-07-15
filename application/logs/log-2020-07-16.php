8f7c64401931c22dc8fc3daeea849178c0dd2f50
linerec <linerec@naver.com>
2020-07-15 18:06:08 -0400

    Translate

---
+++ application/logs/log-2020-07-16.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-07-16 00:00:32 --> Severity: Notice  --> Trying to get property 'form_name' of non-object /Users/owenkweon/Projects/bubanc/application/views/admin/forms/edit.php 73
+ERROR - 2020-07-16 00:00:32 --> Severity: Notice  --> Trying to get property 'fields_order_primary' of non-object /Users/owenkweon/Projects/bubanc/application/views/admin/forms/edit.php 88
--- templates/selio/widgets/top_explore_features4x_html.php
+++ templates/selio/widgets/top_explore_features4x_html.php
@@ -14,16 +14,16 @@ Widget-preview-image: /assets/img/widgets_preview/top_explore_features4x_html.we
             </div>
         </div>
         <div class="row">
-            <div class="col-xl-3 col-sm-6 col-md-6 col-lg-4">
+            <div class="col-xl-3 col-sm-6 col-md-3 col-lg-3">
                 <a href="#" title="">
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-4 col-sm-5 col-3">
+                                <div class="col-md-4 col-sm-3 col-3">
                                     <i class="icon-home hidden"></i>
                                     <img src="assets/img/icons/financial/finance_icon_01.png" alt="Financial_01" />
                                 </div>
-                                <div class="col-md-8 col-sm-7 col-9">
+                                <div class="col-md-8 col-sm-9 col-9">
                                     <h3><?php echo lang_check('Perfect Tools');?></h3>
                                     <p><?php echo lang_check('explore2_perfect_description');?></p>
                                 </div>
@@ -32,16 +32,16 @@ Widget-preview-image: /assets/img/widgets_preview/top_explore_features4x_html.we
                     </div><!--card end-->
                 </a>
             </div>
-            <div class="col-xl-3 col-sm-6 col-md-6 col-lg-4">
+            <div class="col-xl-3 col-sm-6 col-md-3 col-lg-3">
                 <a href="#" title="">
                     <div class="card">
                         <div class="card-body">
                         <div class="row">
-                                <div class="col-md-4 col-sm-5 col-3">
+                                <div class="col-md-4 col-sm-3 col-3">
                                     <i class="icon-cursor hidden"></i>
                                     <img src="assets/img/icons/financial/finance_icon_02.png" alt="Financial_02" />
                                 </div>
-                                <div class="col-md-8 col-sm-7 col-9">
+                                <div class="col-md-8 col-sm-9 col-9">
                                     <h3><?php echo lang_check('Search in Click');?></h3>
                                     <p><?php echo lang_check('explore2_perfect_search');?></p>
                                 </div>
@@ -50,16 +50,16 @@ Widget-preview-image: /assets/img/widgets_preview/top_explore_features4x_html.we
                     </div>
                 </a>
             </div>
-            <div class="col-xl-3 col-sm-6 col-md-6 col-lg-4">
+            <div class="col-xl-3 col-sm-6 col-md-3 col-lg-3">
                 <a href="#" title="">
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-4 col-sm-5 col-3">
+                                <div class="col-md-4 col-sm-3 col-3">
                                     <i class="icon-lock hidden"></i>
                                     <img src="assets/img/icons/financial/finance_icon_03.png" alt="Financial_03" />
                                 </div>
-                                <div class="col-md-8 col-sm-7 col-9">
+                                <div class="col-md-8 col-sm-9 col-9">
                                     <h3><?php echo lang_check('User Control');?></h3>
                                     <p><?php echo lang_check('explore2_perfect_control');?></p>
                                 </div>
@@ -68,16 +68,16 @@ Widget-preview-image: /assets/img/widgets_preview/top_explore_features4x_html.we
                     </div>
                 </a>
             </div>
-            <div class="col-xl-3 col-sm-6 col-md-6 col-lg-4">
+            <div class="col-xl-3 col-sm-6 col-md-3 col-lg-3">
                 <a href="#" title="">
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-4 col-sm-5 col-3">
+                                <div class="col-md-4 col-sm-3 col-3">
                                     <i class="la la-star-o hidden"></i>
                                     <img src="assets/img/icons/financial/finance_icon_04.png" alt="Financial_04" />
                                 </div>
-                                <div class="col-md-8 col-sm-7 col-9">
+                                <div class="col-md-8 col-sm-9 col-9">
                                     <h3 class=""><?php echo lang_check('5 Star Support');?></h3>
                                     <p class=""><?php echo lang_check('explore2_support');?></p>
                                 </div>
