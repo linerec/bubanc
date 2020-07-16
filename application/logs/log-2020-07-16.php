9874c550a641f269008c0e2f2967f98dc2f7bc46
linerec <linerec@naver.com>
2020-07-16 03:00:09 -0400

    Update Language Settings and Favicons

--- application/logs/log-2020-07-16.php
+++ application/logs/log-2020-07-16.php
@@ -2,3 +2,19 @@
 
 ERROR - 2020-07-16 00:00:32 --> Severity: Notice  --> Trying to get property 'form_name' of non-object /Users/owenkweon/Projects/bubanc/application/views/admin/forms/edit.php 73
 ERROR - 2020-07-16 00:00:32 --> Severity: Notice  --> Trying to get property 'fields_order_primary' of non-object /Users/owenkweon/Projects/bubanc/application/views/admin/forms/edit.php 88
+ERROR - 2020-07-16 00:13:15 --> Severity: Warning  --> session_start(): open(/var/tmp//sess_0fchhk2a8jk2irrcdv0sp9n6fh, O_RDWR) failed: Permission denied (13) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:13:15 --> Severity: Warning  --> session_start(): Failed to read session data: files (path: ) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:13:35 --> Severity: Warning  --> session_start(): open(/var/tmp//sess_0fchhk2a8jk2irrcdv0sp9n6fh, O_RDWR) failed: Permission denied (13) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:13:35 --> Severity: Warning  --> session_start(): Failed to read session data: files (path: ) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:15:43 --> Severity: Warning  --> session_start(): open(/var/tmp//sess_0fchhk2a8jk2irrcdv0sp9n6fh, O_RDWR) failed: Permission denied (13) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:15:43 --> Severity: Warning  --> session_start(): Failed to read session data: files (path: ) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:15:51 --> Severity: Warning  --> session_start(): open(/var/tmp//sess_0fchhk2a8jk2irrcdv0sp9n6fh, O_RDWR) failed: Permission denied (13) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:15:51 --> Severity: Warning  --> session_start(): Failed to read session data: files (path: ) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:16:12 --> Severity: Warning  --> session_start(): open(/var/tmp//sess_0fchhk2a8jk2irrcdv0sp9n6fh, O_RDWR) failed: Permission denied (13) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:16:12 --> Severity: Warning  --> session_start(): Failed to read session data: files (path: ) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:18:13 --> Severity: Warning  --> session_start(): open(/var/tmp//sess_0fchhk2a8jk2irrcdv0sp9n6fh, O_RDWR) failed: Permission denied (13) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 00:18:13 --> Severity: Warning  --> session_start(): Failed to read session data: files (path: ) /Users/owenkweon/Projects/bubanc/application/views/admin/components/page_tail_main.php 40
+ERROR - 2020-07-16 08:51:32 --> Severity: Notice  --> Undefined variable: config_base_url /Users/owenkweon/Projects/bubanc/application/config/config.php 30
+ERROR - 2020-07-16 08:51:54 --> Severity: Notice  --> Undefined variable: config_base_url /Users/owenkweon/Projects/bubanc/application/config/config.php 30
+ERROR - 2020-07-16 08:52:04 --> Severity: Notice  --> Undefined variable: config_base_url /Users/owenkweon/Projects/bubanc/application/config/config.php 30
+ERROR - 2020-07-16 08:53:00 --> Severity: Notice  --> Undefined variable: config_base_url /Users/owenkweon/Projects/bubanc/application/config/config.php 30
